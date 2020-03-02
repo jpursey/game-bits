19a5ff810d031b2d893887a6a98aa73f20464dd9
John Pursey <john.pursey@gmail.com>
2020-03-01 20:50:18 -0800

    Make GameLoop timing less convoluted

--- src/gbits/game/game.cc
+++ src/gbits/game/game.cc
@@ -29,56 +29,49 @@ void Game::GameLoop() {
     min_delta_time = absl::Nanoseconds(1000000000LL / max_fps);
   }
 
-  // Last time is used to determine when min_delta_time is satisfied.
   absl::Time last_time = absl::Now();
-
-  // Cumulative time is the actual amount of time that has past.
-  absl::Time cumulative_time = last_time;
-
-  // Delta is how much time has passed since cumulative time.
-  absl::Duration delta;
+  absl::Time next_time = last_time + min_delta_time;
+  absl::Duration delta_time;
   do {
-    delta = absl::Now() - cumulative_time;
-
-    // To check against min_delta_time, we use a delta based on last time (which
-    // is strictly the same or earlier than cumulative_time).
-    absl::Duration test_delta = delta + (cumulative_time - last_time);
-
-    if (min_delta_time == absl::ZeroDuration()) {
-      last_time += delta;
-    } else if (test_delta > min_delta_time) {
-      // The game is running slower than the desired frame rate. If it is close
-      // (within a millisecond), then we will try to absorb the time in the next
-      // frame (it may be a one off issue). Otherwise, we just eat the cost in
-      // this frame, and give the next frame more time. This is important when a
-      // single frame may take many seconds (for instance loading a level).
-      if (test_delta - min_delta_time < absl::Milliseconds(1)) {
-        last_time += min_delta_time;
+    absl::Time now = absl::Now();
+    delta_time = now - last_time;
+    absl::Duration time_remaining = next_time - now;
+    if (time_remaining < absl::ZeroDuration()) {
+      // The game is running slower than the desired frame rate (or the frame
+      // rate is unlocked). If it is close (within a millisecond), then we will
+      // try to absorb the time in the next frame (it may be a one off issue).
+      // Otherwise, we just eat the cost in this frame, and give the next frame
+      // more time. This is important when a single frame may take many seconds
+      // (for instance loading a level).
+      if (time_remaining < -absl::Milliseconds(1)) {
+        next_time += min_delta_time;
       } else {
-        last_time += test_delta;
+        next_time = now;
       }
     } else {
       // We have extra time on our hands. Be nice first, and yield time to the
       // system if we need to wait more than a millisecond.
-      while (min_delta_time - test_delta > absl::Milliseconds(1)) {
-        absl::SleepFor(min_delta_time - test_delta);
-        test_delta = absl::Now() - last_time;
+      while (time_remaining > absl::Milliseconds(1)) {
+        absl::SleepFor(time_remaining);
+        now = absl::Now();
+        time_remaining = next_time - now;
       }
 
       // Busy loop the rest of the time, if there is any.
-      while (test_delta < min_delta_time) {
-        test_delta = absl::Now() - last_time;
+      while (time_remaining > absl::ZeroDuration()) {
+        now = absl::Now();
+        time_remaining = next_time - now;
       }
 
       // Calculate the actual delta.
-      delta = test_delta - (cumulative_time - last_time);
+      delta_time = now - last_time;
 
       // Advance last time by min_delta_time so we can maintain a reliable frame
       // rate. Otherwise we will drift slightly slower than the desired rate.
-      last_time += min_delta_time;
+      next_time += min_delta_time;
     }
-    cumulative_time += delta;
-  } while (Update(delta));
+    last_time = now;
+  } while (Update(delta_time));
 }
 
 }  // namespace gb
