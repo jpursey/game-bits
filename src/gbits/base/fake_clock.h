85dbbd605661600c6ee6b85a1f4231cd06de56c3
John Pursey <john.pursey@gmail.com>
2020-03-03 21:41:09 -0800

    Add Clock classes

---
+++ src/gbits/base/fake_clock.h
@@ -0,0 +1,37 @@
+#ifndef GBITS_BASE_FAKE_CLOCK_H_
+#define GBITS_BASE_FAKE_CLOCK_H_
+
+#include "gbits/base/clock.h"
+
+namespace gb {
+
+class FakeClock : public Clock {
+ public:
+  FakeClock() = default;
+  ~FakeClock() override = default;
+
+  absl::Time Now() override {
+    now_ += auto_advance_;
+    return now_;
+  }
+  void SleepFor(absl::Duration duration) override { now_ += duration + sleep_offset_; }
+
+  absl::Time GetTime() { return now_; }
+  void SetTime(absl::Time now) { now_ = now; }
+  void AdvanceTime(absl::Duration duration) { now_ += duration; }
+  void SetAutoAdvance(absl::Duration auto_advance) {
+    auto_advance_ = auto_advance;
+  }
+  void SetSleepOffset(absl::Duration sleep_offset) {
+    sleep_offset_ = sleep_offset;
+  }
+
+ private:
+  absl::Time now_;
+  absl::Duration auto_advance_;
+  absl::Duration sleep_offset_;
+};
+
+}  // namespace gb
+
+#endif  // GBITS_BASE_FAKE_CLOCK_H_
