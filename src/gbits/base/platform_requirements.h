bdbad198739be58270f72a2aab75bfe91969858c
John Pursey <john.pursey@gmail.com>
2020-04-19 15:36:50 -0700

    Add platform requirements
    C++ supports an incredibly broad set of platforms, including esoteric
    character encodings, integer sizes, and such. The gbits libraries do not
    support platforms with more exotic features. This ensures that gbits will
    explicitly not compile for those platforms.

---
+++ src/gbits/base/platform_requirements.h
@@ -0,0 +1,10 @@
+#ifndef GBITS_BASE_PLATFORM_REQUIREMENTS_H_
+#define GBITS_BASE_PLATFORM_REQUIREMENTS_H_
+
+namespace gb {
+
+bool IsPlatformSupported();
+
+}  // namespace gb
+
+#endif  // GBITS_BASE_PLATFORM_REQUIREMENTS_H_
