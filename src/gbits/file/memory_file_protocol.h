9805e218fe03f43a467a7210ecd0fd9d164e3aa4
John Pursey <john.pursey@gmail.com>
2020-05-25 10:19:00 -0700

    Add MemoryFileProtocol
    This is a thread-safe protocol that hosts the entire file system in
    memory.

---
+++ src/gbits/file/memory_file_protocol.h
@@ -0,0 +1,59 @@
+#ifndef GBITS_FILE_MEMORY_FILE_PROTOCOL_H_
+#define GBITS_FILE_MEMORY_FILE_PROTOCOL_H_
+
+#include <map>
+#include <memory>
+
+#include "absl/synchronization/mutex.h"
+#include "gbits/base/string_util.h"
+#include "gbits/file/file_protocol.h"
+
+namespace gb {
+
+// This class implements a file protocol interface using heap memory.
+//
+// This class supports all file system operations, allocating memory as needed
+// from the heap. By default, it will register under the "mem" protocol name.
+//
+// This class is thread-safe.
+class MemoryFileProtocol : public FileProtocol {
+ public:
+  explicit MemoryFileProtocol(FileProtocolFlags flags = kAllFileProtocolFlags);
+  ~MemoryFileProtocol() override;
+
+  // Public overrides for FileProtocol.
+  FileProtocolFlags GetFlags() const override;
+  std::vector<std::string> GetDefaultNames() const override;
+
+ protected:
+  // Protected overrides for FileProtocol.
+  void Lock(LockType type) override;
+  void Unlock(LockType type) override;
+  PathInfo DoGetPathInfo(std::string_view protocol_name,
+                         std::string_view path) override;
+  std::vector<std::string> BasicList(std::string_view protocol_name,
+                                     std::string_view path) override;
+  bool BasicCreateFolder(std::string_view protocol_name,
+                         std::string_view path) override;
+  bool BasicDeleteFolder(std::string_view protocol_name,
+                         std::string_view path) override;
+  bool BasicDeleteFile(std::string_view protocol_name,
+                       std::string_view path) override;
+  std::unique_ptr<RawFile> BasicOpenFile(std::string_view protocol_name,
+                                         std::string_view path,
+                                         FileFlags flags) override;
+
+ private:
+  class MemoryFile;
+  struct Node;
+  using Nodes = std::map<std::string, std::unique_ptr<Node>, StringKeyCompare>;
+
+  const FileProtocolFlags flags_;
+
+  absl::Mutex mutex_;
+  Nodes nodes_;
+};
+
+}  // namespace gb
+
+#endif  // GBITS_FILE_MEMORY_FILE_PROTOCOL_H_
