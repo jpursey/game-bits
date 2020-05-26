291eca06e6e717250bf0003026c7191dcb13bdeb
John Pursey <john.pursey@gmail.com>
2020-05-26 00:28:41 -0700

    Add GenerateAlphaTestString test function
    This is the same as GenerateTestString, but limits the characters to only
    lowercase ASCII letters. These strings can be used for filenames or other
    restricted name types.

--- src/gbits/test/test_util.cc
+++ src/gbits/test/test_util.cc
@@ -17,4 +17,17 @@ std::string GenerateTestString(int64_t size) {
   return text;
 }
 
+std::string GenerateAlphaTestString(int64_t size) {
+  // True randomness is unimportant here, but we make sure that no two adjacent
+  // characters are equal to better detect off-by-one errors.
+  std::string text(size, 'a' + static_cast<char>(std::rand() % 26));
+  for (std::string::size_type i = 1; i < text.size(); ++i) {
+    text[i] = 'a' + static_cast<char>(std::rand() % 25);
+    if (text[i] == text[i - 1]) {
+      ++text[i];
+    }
+  }
+  return text;
+}
+
 }  // namespace gb
--- src/gbits/test/test_util.h
+++ src/gbits/test/test_util.h
@@ -10,6 +10,11 @@ namespace gb {
 // help detect off-by-one errors).
 std::string GenerateTestString(int64_t size);
 
+// Generates a pseudo-random ASCII string of the specified size containing only
+// lowercase characters. It further guarantees that no two adjacent characters
+// in the string are the same (to help detect off-by-one errors).
+std::string GenerateAlphaTestString(int64_t size);
+
 }  // namespace gb
 
 #endif  // GBITS_TEST_TEST_UTIL_H_
