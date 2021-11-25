cf16246b1c142f2e56f9780d69bd2eebfb5914b2
John Pursey <john.pursey@gmail.com>
2021-11-25 13:59:15 -0800

    Image: Add conversion from glm vec to Pixel

--- src/gb/image/pixel.h
+++ src/gb/image/pixel.h
@@ -11,6 +11,7 @@
 #include <algorithm>
 
 #include "gb/image/image_types.h"
+#include "glm/glm.hpp"
 
 namespace gb {
 
@@ -22,6 +23,16 @@ struct alignas(uint32_t) Pixel {
   // Default opaque colored pixel.
   constexpr Pixel(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255)
       : r(r), g(g), b(b), a(a) {}
+  constexpr explicit Pixel(const glm::vec3& color)
+      : r(static_cast<uint8_t>(color.r * 255.0f)),
+        g(static_cast<uint8_t>(color.g * 255.0f)),
+        b(static_cast<uint8_t>(color.b * 255.0f)),
+        a(255) {}
+  constexpr explicit Pixel(const glm::vec4& color)
+      : r(static_cast<uint8_t>(color.r * 255.0f)),
+        g(static_cast<uint8_t>(color.g * 255.0f)),
+        b(static_cast<uint8_t>(color.b * 255.0f)),
+        a(static_cast<uint8_t>(color.a * 255.0f)) {}
 
   // Direct from a packed pixel value.
   explicit Pixel(uint32_t packed)
